5c569fa2401cc54776b7e2072d6649d42cd27bf1
steiner <steiner3044@163.com>
2022-09-17 15:47:15 +0800

    flutter frontend

---
+++ flutter_frontend/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/globalstate.dart';
+import 'package:flutter_frontend/models.dart';
+import 'package:flutter_frontend/pages.dart';
+import 'package:flutter_frontend/widgets.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ChangeNotifierProvider(
+      create: (_) => GlobalState(),
+      child: MaterialApp(
+        title: "App",
+        home: LoginPage(),
+      )
+    );
+
+  }
+}
+
+
+class TestPage extends StatelessWidget {
+  final postshortcutWidget = PostShortcutWidget(
+    shortcut: PostShortcut(
+        postid: 1,
+        title: "hello",
+        shortcut: "<h5>hello world</h5>" * 200,
+        author: User(id: 1, name: "hello", email: "hello"),
+        category: Category(id: 1, name: "category1")
+    ),
+  );
+
+  final postWidget = PostView(
+      post: Post(
+        id: 1,
+        title: "hello",
+        body: "<h1>Hello World</h1>" * 5,
+        category: Category(id: 1, name: "category 1"),
+        author: User(id: 1, name: "steiner", email: "email"),
+        timestamp: '',
+        comments: <Comment>[],
+      )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("Test Page"),),
+      body: Center(
+        child: postshortcutWidget
+      )
+    );
+  }
+}
